9e881f924b2ca0ed822c61780565920b4b212b26
Guilherme Casimiro <guircasimiro@gmail.com>
2018-07-19 19:10:59 -0300

    refactoring code and correct neutral score issue

--- sentiment.py
+++ sentiment.py
@@ -12,45 +12,78 @@ def retrieve_comments(sub_id):
     return comm_cur
 
 
+def prepare_sent_analyzer():
+    with open('subreddit_sentiment.csv', 'a', newline='') as csvfile:
+            field_names = ['Name', 'Positive', 'Negative', 'Neutral', 'Compound']
+            writer = csv.DictWriter(csvfile, fieldnames=field_names)
+            writer.writeheader()
+
+
+def write_sent_analyzer(sub_eval, name):
+    with open('subreddit_sentiment.csv', 'a', newline='') as csvfile:
+            field_names = ['Name', 'Positive', 'Negative', 'Neutral', 'Compound']
+            writer = csv.DictWriter(csvfile, fieldnames=field_names)
+            writer.writerow({'Name': name,
+                             'Positive': sub_eval['pos'],
+                             'Negative': sub_eval['neg'],
+                             'Neutral': sub_eval['neu'],
+                             'Compound': sub_eval['compound']})
+
+
+def sum_vader(dict_eval, dict_score):
+    dict_eval['neg'] += dict_score['neg']
+    dict_eval['pos'] += dict_score['pos']
+    dict_eval['neu'] += dict_score['neu']
+    dict_eval['compound'] += dict_score['compound']
+    return dict_eval
+
+
+def div_vader(dict_eval, length):
+    dict_eval['neg'] /= length
+    dict_eval['pos'] /= length
+    dict_eval['neu'] /= length
+    dict_eval['compound'] /= length
+    return dict_eval
+
+def give_score_vader(sentence_list):
+    """Gives scores average for sentences in a comment"""
+    sid = SentimentIntensityAnalyzer()
+    sent_eval = {"neg": 0, "pos": 0, "neu": 0, 'compound': 0}
+    for sentence in sentence_list:
+        sid_score = sid.polarity_scores(sentence)
+        sent_eval = sum_vader(sent_eval, sid_score)
+    if len(sentence_list) == 0:
+        print('zerado')
+        print(sentence_list)
+    sent_eval = div_vader(sent_eval, len(sentence_list))
+    return sent_eval
+
+
 def comm_sent_analyzer(sub_comments):
     """Analyses sentiment in comments per subreddit."""
-    sid = SentimentIntensityAnalyzer()
+    prepare_sent_analyzer()
     sub_eval = {}
     for sub in sub_comments:
         sub_eval[sub[1]] = {"neg": 0, "pos": 0, "neu": 0, 'compound': 0}
         sub_sentences = []
         print('Read comments')
+        total_comm = 0
         for comment in retrieve_comments(sub[0]):
             # It divides comments in sentences
             sentence_list = tokenize.sent_tokenize(comment[0])
-            sub_sentences.extend(sentence_list)
-        print('Give sentence scores')
-        for sentence in sub_sentences:
-            # It gives score for each sentence
-            sid_score = sid.polarity_scores(sentence)
-            sub_eval[sub[1]]['neg'] += sid_score['neg']
-            sub_eval[sub[1]]['pos'] += sid_score['pos']
-            sub_eval[sub[1]]['neu'] += sid_score['neu']
-            sub_eval[sub[1]]['compound'] += sid_score['compound']
-        sub_eval[sub[1]]['neg'] /= len(sub_sentences)
-        sub_eval[sub[1]]['pos'] /= len(sub_sentences)
-        sub_eval[sub[1]]['neu'] /= len(sub_sentences)
-        sub_eval[sub[1]]['compound'] /= len(sub_sentences)
-        print('Write ' + sub[1])
-        with open('subreddit_sentiment.csv', 'w+', newline='') as csvfile:
-            field_names = ['Name', 'Positive', 'Negative', 'Neutral', 'Compound']
-            writer = csv.DictWriter(csvfile, fieldnames=field_names)
-            writer.writeheader()
-            writer.writerow({'Name': sub[1],
-                             'Positive': sub_eval[sub[1]]['pos'],
-                             'Negative': sub_eval[sub[1]]['neg'],
-                             'Neutral': sub_eval[sub[1]]['neg'],
-                             'Coumpond': sub_eval[sub[1]]['coumpond']})
-
+            if len(sentence_list) == 0:
+                print('vazio')
+                continue
+            eval_comm = give_score_vader(sentence_list)
+            sub_eval[sub[1]] = sum_vader(sub_eval[sub[1]], eval_comm)
+            total_comm += 1
+        print(total_comm)
+        sub_eval[sub[1]] = div_vader(sub_eval[sub[1]], total_comm)
+        write_sent_analyzer(sub_eval[sub[1]], sub[1])
     return sub_eval
 
 
 CONN = connect.connect('reddit_test', 'guilhermecasimiro', 'grc_2018')
 CUR = CONN.cursor()
-CUR.execute("SELECT * from subreddit limit 1")
+CUR.execute("SELECT * from subreddit WHERE name = 'religion' OR name = 'science'")
 result = comm_sent_analyzer(CUR.fetchall())
